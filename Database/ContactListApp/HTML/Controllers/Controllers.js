4290405d5f967e76822e1f8f53f0d0fd15e23b97
Alice Yu <aljyu@ucsc.edu>
2015-10-07 19:12:53 -0700

    Updated code

--- Database/ContactListApp/HTML/Controllers/Controllers.js
+++ Database/ContactListApp/HTML/Controllers/Controllers.js
@@ -1,11 +1,7 @@
 var myApp = angular.module("myApp", []);
-myApp.controller("AppCtrl", ["$scope", "$http", function($scope, $http) {
-	console.log("Hello World from the controller");
-	
+myApp.controller("AppCtrl", ["$scope", "$http", function($scope, $http) {	
 	var refresh = function() {
 		$http.get("/contactlist").success(function(response) {
-			//console.log will tell us if we succcessfully got the data we requested
-			console.log("I got the data I requested");
 			//Puts the data into our HTML file
 			$scope.contactlist = response;
 			$scope.contact = "";
@@ -16,17 +12,30 @@ myApp.controller("AppCtrl", ["$scope", "$http", function($scope, $http) {
 
 	$scope.addContact = function() {
 		$http.post('/contactlist', $scope.contact).success(function(response){
-				console.log(response);
-				refresh();
-			});
+			refresh();
+		});
 	};
 
 	$scope.remove = function(id) {
-  		console.log(id);
   		$http.delete('/contactlist/' + id).success(function(response) {
-    	refresh();
-  		});
+			refresh();
+		});
+	};
+	
+	$scope.edit = function(id) {
+		$http.get("/contactlist/" + id).success(function(response) {
+			$scope.contact = response;
+			refresh();
+		});
+	};
+	
+	$scope.update = function() {
+		$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+			refresh();
+		})
 	};
 
-
+	$scope.deselect = function() {
+		$scope.contact = "";
+	}
 }]);
